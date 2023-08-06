954fd49bc94aa511f344a8b526dd50b25b567651
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2023-08-06 22:47:15 +0200

    Use plain nodejs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { CardSerializer } from "$lib/anki/card-serializer";
+	import { MediaFolderProvider } from "$lib/anki/media-folder-provider";
+
+    let dirs = MediaFolderProvider.getMediaFolders()
+    // import fs from "fs"
+    // let s = fs.createWriteStream("foo.txt");
+    // CardSerializer.serialize(s, [{front: "FOO", back: "Bar", tags: ["1", "2"]}]);
+</script>
+
+<h1>Test</h1>
+
+<ul>
+    {#each dirs as dir }
+        <li><code>{dir}</code></li>
+    {/each}
+</ul>

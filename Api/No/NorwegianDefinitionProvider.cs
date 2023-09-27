3626e9e4ba0a3f726138ae0e7821dff1060b52b9
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2023-09-27 09:13:31 +0200

    Generate client differently

--- Api/No/NorwegianDefinitionProvider.cs
+++ Api/No/NorwegianDefinitionProvider.cs
@@ -1,22 +1,24 @@
-using AngleSharp;
-using AngleSharp.Dom;
-using AngleSharp.Io;
+using System.Diagnostics;
+using Libraian.Api.Clients;
 
 namespace Librarian.Api.No;
 
 public class NorwegianDefinitionProvider
 {
-    // private readonly ILogger _logger;
-    private readonly IHttpClientFactory _factory;
+    private readonly ILogger<NorwegianDefinitionProvider> _logger;
+    private readonly OrdbokClient _client;
 
-    public NorwegianDefinitionProvider(IHttpClientFactory factory)
+    public NorwegianDefinitionProvider(ILogger<NorwegianDefinitionProvider> logger, OrdbokClient client)
     {
-        // _logger = logger;
-        _factory = factory;
+        _logger = logger;
+        _client = client;
     }
-    
-    public async Task<string> GetDefinitionAsync(string lemma, CancellationToken token)
+
+    public async Task<string> GetDefinitionsAsync(string word)
     {
-        return "test";
+        _logger.LogInformation("Begin");
+        var response = await _client.ArticlesAsync(word, "bm", "", "eif");
+        _logger.LogInformation("Result {Result}", response.Articles.Bm.Count);
+        return string.Empty;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-using AngleSharp.Dom;
+using Libraian.Api.Clients;
 using Librarian.Api.No;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient("naob",
-    client => client.BaseAddress = new Uri("https://naob.no/ordbok/"));
+builder.Services.AddHttpClient();
 
+builder.Services.AddTransient<OrdbokClient>();
 builder.Services.AddTransient<NorwegianDefinitionProvider>();
 
 var app = builder.Build();
@@ -15,7 +15,7 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/api/definition/{word}",
-    ([FromRouteAttribute] string word, NorwegianDefinitionProvider pr) =>
-        Task.FromResult($"{word}{word}"));
+    async ([FromRouteAttribute] string word, NorwegianDefinitionProvider pr) =>
+        await pr.GetDefinitionsAsync(word));
 
 app.Run();

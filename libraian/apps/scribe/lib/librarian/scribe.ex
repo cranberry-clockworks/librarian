9e39e19d8bb75eb51ed26f5b9c1279e75e512093
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2025-09-10 21:19:15 +0200

    !fixme

--- libraian/apps/scribe/lib/librarian/scribe.ex
+++ libraian/apps/scribe/lib/librarian/scribe.ex
@@ -1,18 +1,16 @@
 defmodule Librarian.Scribe do
-  require EEx
-
   def main(argv) do
     with {:ok, template_path} <- parse_arguments(argv),
          {:ok, template} <- File.read(template_path),
          input <- IO.read(:stdio, :eof),
-         {:ok, entries} <- Jason.decode(input) do
+         {:ok, entries} <- Jason.decode(input),
+         true <- is_list(entries) do
       for entry <- entries do
         EEx.eval_string(template, assigns: entry) |> IO.puts()
       end
     else
       {:error, reason} -> IO.puts(reason)
     end
-  
   end
 
   defp parse_arguments(argv) do
